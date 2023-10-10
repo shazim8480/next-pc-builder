fddbecd307ebba6c61d905925c0377ae4d4c5e3e
shazim8480 <troublemaker8480@gmail.com>
2023-10-11 00:03:24 +0600

    ssg add

--- src/pages/cpu/index.js
+++ src/pages/cpu/index.js
@@ -16,9 +16,9 @@ CPUPage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
 };
 
-export async function getStaticProps() {
-  const res = await fetch("http://localhost:8000/products");
-  const products = await res.json();
-  console.log(products);
-  return { props: { products } };
-}
+// export async function getStaticProps() {
+//   const res = await fetch("http://localhost:8000/products");
+//   const products = await res.json();
+//   console.log(products);
+//   return { props: { products } };
+// }
